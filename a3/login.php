bbc31e41233f115adac192373bf10498cecd96e1
Viktor <vikabr0524@gmail.com>
2025-12-01 11:02:21 +0100

    sync

--- a3/login.php
+++ a3/login.php
@@ -1,28 +1,42 @@
-<?php
-include('template.php');
-if (isset($_POST['username']) and isset($_POST['password'])) {
-  $query = <<<END
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  include('template.php');
+  if (isset($_POST['username']) and isset($_POST['password'])) {
+    $query = <<<END
 SELECT username, password, id FROM users
 WHERE username = '{$_POST['username']}'
 AND password = '{$_POST['password']}'
 END;
-  $result = $mysqli->query($query);
-  if ($result->num_rows > 0) {
-    $row = $result->fetch_object();
-    $_SESSION["username"] = $row->username;
-    $_SESSION["userId"] = $row->id;
-    header("Location:index.php");
-  } else {
-    echo "Wrong username or password. Try again";
+    $result = $mysqli->query($query);
+    if ($result->num_rows > 0) {
+      $row = $result->fetch_object();
+      $_SESSION["username"] = $row->username;
+      $_SESSION["userId"] = $row->id;
+      header("Location:index.php");
+    } else {
+      echo "Wrong username or password. Try again";
+    }
   }
-}
-$content = <<<END
+  $content = <<<END
 <form action="login.php" method="post">
 <input type="text" name="username" placeholder="username">
 <input type="password" name="password" placeholder="password">
 <input type="submit" value="Login">
 </form>
 END;
-echo $navigation;
-echo $content;
-?>
+  echo $navigation;
+  echo $content;
+  ?>
+
+</body>
+
+</html>
--- a3/template.php
+++ a3/template.php
@@ -1,13 +1,24 @@
-<link rel="stylesheet" property="stylesheet" type="text/css" href="css/stylesheet.css">
-<?php
-session_name('Website');
-session_start();
-$host = "localhost";
-$user = "vikabr24"; // e.g. evanil18
-$pwd = "m5icdOQDGp"; // e.g takeAbath@06h30
-$db = "vikabr24"; // e.g wagdem20_db
-$mysqli = new mysqli($host, $user, $pwd, $db);
-$navigation = <<<END
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" property="stylesheet" type="text/css" href="css/stylesheet.css">
+
+</head>
+
+<body>
+  <?php
+  session_name('Website');
+  session_start();
+  $host = "localhost";
+  $user = "vikabr24"; // e.g. evanil18
+  $pwd = "m5icdOQDGp"; // e.g takeAbath@06h30
+  $db = "vikabr24"; // e.g wagdem20_db
+  $mysqli = new mysqli($host, $user, $pwd, $db);
+  $navigation = <<<END
 <nav>
 <a href="index.php">Home</a>
 <a href="about.php">About</a>
@@ -17,21 +28,24 @@ $navigation = <<<END
 <a href="product_details.php">Product details</a>
 
 END;
-if (isset($_SESSION['userId'])) {
-  $navigation .= <<<END
+  if (isset($_SESSION['userId'])) {
+    $navigation .= <<<END
 <a href="logout.php">Logout</a>
 Logged in as {$_SESSION['username']}
 END;
-}
+  }
 
-if (!isset($_SESSION['userId'])) {
-  $navigation .= <<<END
+  if (!isset($_SESSION['userId'])) {
+    $navigation .= <<<END
     <a href="login.php">Log in</a>
     <a href="register.php">Register</a>
   END;
-}
+  }
 
 
 
-$navigation .= '</nav>';
-?>
+  $navigation .= '</nav>';
+  ?>
+</body>
+
+</html>
